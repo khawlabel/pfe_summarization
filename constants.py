7eddd31a7f7336af42dbbab9139fbf9d9c0cf2b4
khawla belgacem <k_belgacem@estin.dz>
2025-04-27 20:28:36 +0100

    ..

--- constants.py
+++ constants.py
@@ -8,11 +8,14 @@ loaded = load_dotenv()
 # Récupérer les clés API de manière sécurisée
 QDRANT_API = os.getenv('QDRANT_API')
 QDRANT_URL = os.getenv('QDRANT_URL')
-
+GROQ_API_KEY_3=os.getenv('GROQ_API_KEY_3')
 GROQ_API_KEY = os.getenv('GROQ_API_KEY')
 
-
+GROQ_API_KEY_2 = os.getenv('GROQ_API_KEY_2')
+GROQ_API_KEY_4 = os.getenv('GROQ_API_KEY_4')
 QDRANT_COLLECTION = os.getenv('QDRANT_COLLECTION')
+QDRANT_COLLECTION_v1 = os.getenv('QDRANT_COLLECTION_v1')
+
 
 # Modèles utilisés
 LLM_NAME_1="llama3-8b-8192"
@@ -23,6 +26,8 @@ LLM_NAME_4="llama-3.3-70b-versatile"
 WHISPER="whisper-large-v3"
 MODEL_EMBEDDING="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
   
-  
+MODEL_EMBEDDING_v1="sentence-transformers/all-MiniLM-L6-v2"
+RERANKER_MODEL="BAAI/bge-reranker-v2-m3"
+
 PATH_tesseract = r"C:\Users\Dell\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
 PATH_poppler = "C:/Program Files/poppler-24.08.0/Library/bin"
